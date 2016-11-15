391c2ea20bc3313424cffab19ab40360ac50f26a
nickreitnour <nreit@umich.edu>
2016-11-15 13:44:27 -0500

    Comment test

--- app/src/main/java/com/rosstox26/pathos/projectpathos/MainActivity.java
+++ app/src/main/java/com/rosstox26/pathos/projectpathos/MainActivity.java
@@ -8,6 +8,8 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        //Hi
+
         setContentView(R.layout.activity_main);
     }
 }
