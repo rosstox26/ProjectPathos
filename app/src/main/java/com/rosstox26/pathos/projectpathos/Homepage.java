9b35c13d91230af714d432312d264454da559458
joseberk <joseberk@umich.edu>
2016-11-15 14:07:09 -0500

    Added activities for each of the main screens. At this point we should divide the activities up and create UI's

--- app/src/main/java/com/rosstox26/pathos/projectpathos/Homepage.java
+++ app/src/main/java/com/rosstox26/pathos/projectpathos/Homepage.java
@@ -1,15 +1,13 @@
 package com.rosstox26.pathos.projectpathos;
 
-import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends Activity {
+public class Homepage extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
-
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_homepage);
     }
 }
