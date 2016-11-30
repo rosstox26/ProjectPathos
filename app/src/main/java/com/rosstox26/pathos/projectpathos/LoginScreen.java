2f5cb2a5719fdfb2fb080a21706ebcbf8aebd012
nickreitnour <nreit@umich.edu>
2016-11-30 10:38:03 -0500

    Test on activity_main.xml

--- app/src/main/java/com/rosstox26/pathos/projectpathos/LoginScreen.java
+++ app/src/main/java/com/rosstox26/pathos/projectpathos/LoginScreen.java
@@ -2,7 +2,6 @@ package com.rosstox26.pathos.projectpathos;
 
 import android.app.Activity;
 import android.os.Bundle;
-//import com.facebook.FacebookSdk;
 
 public class LoginScreen extends Activity {
     //comment
