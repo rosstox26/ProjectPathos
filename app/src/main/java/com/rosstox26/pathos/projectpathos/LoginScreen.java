ed5e281d9dcc347aee665e8215ba63e75db97e42
nickreitnour <nreit@umich.edu>
2016-11-30 11:06:25 -0500

    Working on implementing Firebase for Login (working on email right now ... and will do Facebook Login after)

--- app/src/main/java/com/rosstox26/pathos/projectpathos/LoginScreen.java
+++ app/src/main/java/com/rosstox26/pathos/projectpathos/LoginScreen.java
@@ -2,12 +2,42 @@ package com.rosstox26.pathos.projectpathos;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.widget.Button;
+import android.widget.EditText;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class LoginScreen extends Activity {
     //comment
+    private FirebaseAuth mAuth;
+    private FirebaseAuth.AuthStateListener mAuthListener;
+    private EditText editTextEmailAddress;
+    private EditText editTextPassword;
+    private Button buttonLogin;
+    private Button buttonCreateUser;
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login_screen);
+
+        mAuth = FirebaseAuth.getInstance();
+
+        mAuthListener = new FirebaseAuth.AuthStateListener() {
+            @Override
+            public void onAuthStateChanged(@NonNull FirebaseAuth firebaseAuth) {
+                FirebaseUser user = firebaseAuth.getCurrentUser();
+                if (user != null) {
+                    // User is signed in
+                } else {
+                    // User is signed out
+                }
+                // ...
+            }
+        };
+
     }
 }
