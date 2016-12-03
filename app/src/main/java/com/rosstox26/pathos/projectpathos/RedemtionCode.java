2f0f8eed2189073e32a478d477c0eae1bd35b75e
kathleen bird <kathleen@0587338445.wireless.umich.net>
2016-12-03 16:26:15 -0500

    redemption code

---
+++ app/src/main/java/com/rosstox26/pathos/projectpathos/RedemtionCode.java
@@ -0,0 +1,14 @@
+package com.rosstox26.pathos.projectpathos;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class RedemtionCode extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_redemtion_code);
+    }
+
+}
