8e74ce6ddf7d9e315a2e9863e8202dc949dc62a2
nickreitnour <nreit@umich.edu>
2016-12-15 10:37:38 -0500

    Connected Firebase real-time database

--- app/src/main/java/com/rosstox26/pathos/projectpathos/LoginScreen.java
+++ app/src/main/java/com/rosstox26/pathos/projectpathos/LoginScreen.java
@@ -14,7 +14,7 @@ import com.google.android.gms.tasks.Task;
 import com.google.firebase.auth.AuthResult;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
-import com.google.firebase.auth.api.model.StringList;
+//import com.google.firebase.auth.api.model.StringList;
 
 public class LoginScreen extends Activity implements View.OnClickListener {
     //comment
