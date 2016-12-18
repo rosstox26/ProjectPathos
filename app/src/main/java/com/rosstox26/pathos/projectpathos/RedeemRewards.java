e32fb3c5142cc2843e646352f05721a80f5e14ea
kathleen <kathbird@umich.edu>
2016-12-18 10:30:35 -0500

    redemption code

--- app/src/main/java/com/rosstox26/pathos/projectpathos/RedeemRewards.java
+++ app/src/main/java/com/rosstox26/pathos/projectpathos/RedeemRewards.java
@@ -6,20 +6,29 @@ import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
+import android.widget.Button;
 
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 
-public class RedeemRewards extends Activity {
-    //comment
+public class RedeemRewards extends Activity implements View.OnClickListener {
+
+    private Button buttonReward;
+    // /comment
     private FirebaseAuth mAuth;
     private FirebaseAuth.AuthStateListener mAuthListener;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_redeem_rewards);
+
+        buttonReward = (Button) findViewById(R.id.buttonReward);
+        buttonReward.setOnClickListener(this);
+
 
         mAuth = FirebaseAuth.getInstance();
         mAuthListener = new FirebaseAuth.AuthStateListener() {
@@ -90,5 +99,15 @@ public class RedeemRewards extends Activity {
             }
 
             return super.onOptionsItemSelected(item);
+        }
+
+    @Override
+    public void onClick(View view) {
+        Intent toRedemtion= new Intent(Goals.this, RedemtionCode.class);{
+        if (view == buttonReward) {
+            String email = mAuth.getCurrentUser().getEmail();
+            startActivity(toRedemtion);
+        }
+
         }
 }
